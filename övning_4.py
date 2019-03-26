94a542e3c7ff7110209bab397724d4d2fa203f41
ABBFilCed <43879979+ABBFilCed@users.noreply.github.com>
2019-03-26 13:08:31 +0100

    hola

---
+++ övning_4.py
@@ -0,0 +1,44 @@
+#4.1
+
+# lista = range(1000001)
+
+# summa = 0
+
+# for nummer in lista:
+#     summa += nummer
+
+# print(summa)
+
+#4.2
+
+
+# lista = range(1, 500, 2)
+
+# summa = 0
+
+# for nummer in lista:
+#     summa += nummer
+
+# print(summa)
+
+
+#4.3
+
+
+
+# registrerade= ["Anna", "Lars", "Filip", "Oliver", "Knut"]
+# avanmälda= ["Anna", "Lars"]
+
+# for namn in avanmälda:
+#     registrerade.remove(namn)
+
+# print (registrerade)
+
+#4.4
+
+# förnamn = ["Maria", "Erik", "Karl"]
+# efternamn = ["Svensson", "Andersson", "Karlsson"]
+
+# for fnamn in förnamn:
+#     for enamn in efternamn:
+#         print (fnamn + enamn)
---
+++ övning_5.py
@@ -0,0 +1,19 @@
+#5.1
+kön = (input("Ange kön"))
+
+hår = (input("Ange hårfärg"))
+
+Ögon = (input("Ange Ögonfärg"))
+
+if kön = man:
+    if hår = brun:
+        if Ögon = brun:
+            print ("egenskaper matchar med Daniel Radcliffe")
+
+
+if kön = man:
+    if hår = röd:
+        if Ögon = blå:
+            print ("egenskaper matchar med Rupert Grint")
+
+
